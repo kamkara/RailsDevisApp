be4dccfc8a113557ad655f105ce47fee56cc51b9
kamkara <lnclass.kamkara@gmail.com>
2022-04-09 15:36:06 +0000

    add edit and new quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #Onglet title
+    def title
+       title_base = "Lnclass Devis Platform" 
+       if @title.nil?
+         title_base
+       else
+       "#{@title} | #{title_base}"
+       end
+    end
 end
