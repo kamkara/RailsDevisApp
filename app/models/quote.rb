483d59738a49ae78a0e2d873f5339d6636b111d3
kamkara <lnclass.kamkara@gmail.com>
2022-04-10 13:21:07 +0000

    app/models/quote: add real time broadcasting feature with turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -11,8 +11,13 @@ class Quote < ApplicationRecord
     #### METHODE 2 ####
    # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
    
-   #### METHODE 2 ####
-   after_create_commit -> { broadcast_prepend_to "quotes" }
-   after_update_commit -> { broadcast_replace_to "quotes" }
-   after_destroy_commit -> { broadcast_remove_to "quotes" }
+   #### METHODE 2 #### 
+   # later : active le activeJob
+   #after_create_commit -> { broadcast_prepend_later_to "quotes" }
+   #after_update_commit -> { broadcast_replace_later_to "quotes" }
+   #after_destroy_commit -> { broadcast_remove_to "quotes" }
+   
+   #### METHODE 3 ####
+   # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
