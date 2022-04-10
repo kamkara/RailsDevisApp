d36ff45d2dbe4ae6e96ede0d4e761dce91e1df1c
kamkara <lnclass.kamkara@gmail.com>
2022-04-10 12:52:22 +0000

    add real time broadcast with turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,18 @@
 class Quote < ApplicationRecord
-
+    
+    #validates
     validates :name, presence: true
 
+    #ordered scope
     scope :ordered, -> { order(id: :desc) }
+
+    #realtime broadcasting
+    
+    #### METHODE 2 ####
+   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+   
+   #### METHODE 2 ####
+   after_create_commit -> { broadcast_prepend_to "quotes" }
+   after_update_commit -> { broadcast_replace_to "quotes" }
+   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
