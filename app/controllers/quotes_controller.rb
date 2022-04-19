5db032b04a5d76485b4ef6eace9a5c3312844c76
kamkara <lnclass.kamkara@gmail.com>
2022-04-19 16:54:30 +0000

    add flash message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -19,8 +19,8 @@ class QuotesController < ApplicationController
 
     if @quote.save
       respond_to do |format|
-        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created. kara" }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -33,6 +33,7 @@ class QuotesController < ApplicationController
   def update
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully updated."
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
     else
       render :edit, status: :unprocessable_entity
     end
@@ -40,10 +41,9 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-
     respond_to do |format|
       redirect_to quotes_path, notice: "Quote was successfully destroyed."
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
        "#{@title} | #{title_base}"
        end
     end
+
+    #flash message with turbo_stream
+    def render_turbo_stream_flash_messages
+      turbo_stream.prepend "flash", partial: "layouts/flash" 
+    end
 end
